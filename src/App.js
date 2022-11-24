51b21e5827a4c7d7a6f673cd5e1d38d49dcfa978
ThanhHoang <bohoang238@gmail.com>
2022-11-25 02:28:41 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [batdau, hienthi] = useState(1)
+  
+  const tang = () =>{
+    hienthi(batdau + 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{batdau}</h1>
+      console.log(1);
+      <button onClick={tang}>Tăng lên</button>
     </div>
   );
 }
-
+ 
 export default App;
